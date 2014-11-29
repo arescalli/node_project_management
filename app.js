1a42d2255b1a273c0b072800a9014e338e371616
Alessandro Rescalli <alessandro@softrail.it>
2014-11-29 14:19:35 +0100

    added graphical libraries and fix routes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var workRoutes = require('./modules/work/workRoutes');
 var app = express();
 
 // view engine setup
-app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'modules/project/views')]);
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
